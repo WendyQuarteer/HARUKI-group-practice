a97aecd67da3137df9b6451dbae8e36c359a4bdb
Dimitri Bertolami <dimibertolami@gmail.com>
2022-05-01 09:11:42 +0200

    counter badges on buttons

--- dimi.js
+++ dimi.js
@@ -14,8 +14,8 @@ function  Clicked_Victor() {
     $counterBadge2 = document.getElementById('counterBadge2');
     console.log("Victor: " + $counterBadge2.textContent);
     $counterBadge2.textContent = parseInt($counterBadge2.textContent);
-    if ($counterBadge2.textContent<15){
-        $counterBadge2.textContent = parseInt($counterBadge2.textContent)+1;
+    if ($counterBadge2.textContent>0){
+        $counterBadge2.textContent = parseInt($counterBadge2.textContent)-5;
     } else {
         // window.location.href = '/mindandbody.html';
         window.location.href = "/mindandbody.html"
@@ -25,10 +25,13 @@ function  Clicked_Victor() {
 
 function  Clicked_Shiva() {
     $counterBadge3 = document.getElementById('counterBadge3');
+    $counterBadge3.onmouseenter = function (){
+        $counterBadge3.style = "color:bg-light";
+    };
     console.log("Shiva: " + $counterBadge3.textContent);
     $counterBadge3.textContent = parseInt($counterBadge3.textContent);
-    if ($counterBadge3.textContent<13){
-        $counterBadge3.textContent = parseInt($counterBadge3.textContent)+1;
+    if ($counterBadge3.textContent>0){
+        $counterBadge3.textContent = parseInt($counterBadge3.textContent)-1;
     } else {
         // window.location.href = '/contact.html';
         window.location.href = '/contact.html';
